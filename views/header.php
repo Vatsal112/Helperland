747c439c8d48c64202a0946051f38923d56bcbad
Vatsal Dendpara <vatsaldendpara007@gmail.com>
2022-03-06 15:22:10 +0530

    some changes made in header.

--- views/header.php
+++ views/header.php
@@ -25,8 +25,6 @@ session_start();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.js" type="text/javascript"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.js" type="text/javascript"></script>
 
 </head>
 
